7e52d620cc423f1a434b42a192aa457aecca3fac
RamaChintharedd <ramadevi_chinthareddy@epam.com>
2026-03-16 22:15:32 +0530

    Create src/test/java/pages/BasePage.java

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+/**
+ * BasePage provides common utilities to all page objects.
+ * SOLID: Open/Closed - can be extended; Single Responsibility - only common page behavior.
+ */
+public abstract class BasePage {
+    protected final WebDriver driver;
+    protected final WebDriverWait wait;
+
+    protected BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getCurrentUrl() {
+        return driver.getCurrentUrl();
+    }
+}
