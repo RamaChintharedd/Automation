23e0cee2ea6356ba0b57fa267d3be8004bd4a1dc
RamaChintharedd <ramadevi_chinthareddy@epam.com>
2026-03-16 11:45:12 +0530

    Create src/test/java/utils/WaitUtils.java

---
+++ src/test/java/utils/WaitUtils.java
@@ -0,0 +1,42 @@
+package utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+/**
+ * Utility class for explicit waits to keep page objects clean.
+ */
+public class WaitUtils {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public WaitUtils(WebDriver driver, Duration timeout) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, timeout);
+    }
+
+    public WaitUtils(WebDriver driver) {
+        this(driver, Duration.ofSeconds(10));
+    }
+
+    public WebElement waitForVisibility(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public WebElement waitForClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    public boolean waitForUrlContains(String partialUrl) {
+        return wait.until(ExpectedConditions.urlContains(partialUrl));
+    }
+
+    public boolean waitForTextToBePresentInElement(By locator, String text) {
+        return wait.until(ExpectedConditions.textToBePresentInElementLocated(locator, text));
+    }
+}
